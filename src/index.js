4cbcc78d129e58806048862563c5240d22555de1
OlyaMosienko <olya.ololoeva81@mail.ru>
2023-09-15 13:07:13 +0300

    Add burger menu for tab and mob

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'normalize.css'; 
 import './styles/main.scss';
 
+// Smooth Nav Effect
 const links = document.querySelectorAll(".header__link");
         for (const link of links) {
             link.addEventListener("click", clickHandler);
@@ -13,4 +14,11 @@ const links = document.querySelectorAll(".header__link");
             document.querySelector(href).scrollIntoView({
                 behavior: "smooth"
             });
-        }
+        };
+
+// Burger Menu
+let btn = document.querySelector('.burger-btn');
+
+btn.addEventListener('click', function () {
+    this.classList.toggle('is-active');
+});
